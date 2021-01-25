4172137d3ae4e51a157d5cb9599d41d269634c3e
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-01-25 20:57:44 +0530

    add: project files and local database
    update all the project files and database

---
+++ src/test/java/com/iit/cw/CwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.cw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
